dabdd1abeb49f41fb1c1b9a41c1d340e62c03c85
aelw <89507166+aelw@users.noreply.github.com>
2021-10-08 21:35:17 +0900

    Update head.js

--- head.js
+++ head.js
@@ -6,7 +6,7 @@ function getParameterByName(name) {
 }
 var id = getParameterByName('img');
 
-document.write("<div style='white-space: nowrap;width:100%;overflow-x:scroll'><span style='color:#fafafa'><a href='http://stock.1002.pw/dance?img="+id+"'>KPOP</a> / <a href='http://stock.1002.pw/dance/bts?img="+id+"'>BTS</a> / <a href='http://stock.1002.pw/dance/bp?img="+id+"'>Blackpink</a> / <a href='http://stock.1002.pw/dance/itzy?img="+id+"'>Itzy</a> / <a href='http://stock.1002.pw/dance/twice?img="+id+"'>Twice</a> / <a href='http://stock.1002.pw/dance/txt?img="+id+"'>TXT</a></span></div>");
+document.write("<div style='white-space: nowrap;width:100%;overflow-x:scroll'><span style='color:#fafafa'><a href='http://stock.1002.pw/dance?img="+id+"'>KPOP</a> / <a href='http://stock.1002.pw/dance/bts?img="+id+"'>BTS</a> / <a href='http://stock.1002.pw/dance/bp?img="+id+"'>Blackpink</a> / <a href='http://stock.1002.pw/dance/itzy?img="+id+"'>Itzy</a> / <a href='http://stock.1002.pw/dance/twice?img="+id+"'>Twice</a> / <a href='http://stock.1002.pw/dance/txt?img="+id+"'>TXT</a></span></div><br>");
 
 document.write("\
 	  <script async src=\"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9733039422277496\"\n\
