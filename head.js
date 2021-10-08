308904776561db9f6055886f437fb82065a16577
aelw <89507166+aelw@users.noreply.github.com>
2021-10-08 21:47:50 +0900

    Update head.js

--- head.js
+++ head.js
@@ -1,3 +1,7 @@
+document.write("\
+<script async src=\"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9733039422277496\"\n\
+     crossorigin=\"anonymous\"></","script>");
+
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
